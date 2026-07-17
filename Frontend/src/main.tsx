2b4ced1ba6e1f11fc3cefee908aa2bfb2a93d4b5
Sanuka Marasinghe <sanukanm@gmail.com>
2026-07-17 15:00:06 +0530

    feat(pwa): register service worker in entry point

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { initPWA } from "./lib/pwaRegistration";
+
+// Register service worker (no-op if SW is not supported)
+initPWA();
 
 createRoot(document.getElementById("root")!).render(<App />);
